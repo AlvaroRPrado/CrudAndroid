700ca869c462cc4a92ab20acbf2a6867492a055d
root <alvarorodrigues2010@gmail.com>
2023-09-18 12:04:55 -0300

    Listando Subscribers do BD parte 07

--- app/src/main/java/com/prado/crudroom/ui/subscriberlist/SubscriberListFragment.kt
+++ app/src/main/java/com/prado/crudroom/ui/subscriberlist/SubscriberListFragment.kt
@@ -3,28 +3,46 @@ package com.prado.crudroom.ui.subscriberlist
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.prado.crudroom.R
+import com.prado.crudroom.data.db.AppDatabase
 import com.prado.crudroom.data.db.entity.SubscriberEntity
 import com.prado.crudroom.databinding.FragmentSubscriberListBinding
+import com.prado.crudroom.repository.DatabaseDataSource
+import com.prado.crudroom.repository.SubscriberRepository
 
 class SubscriberListFragment : Fragment(R.layout.fragment_subscriber_list) {
 
-    private lateinit var viewModel: SubscriberListViewModel
     private lateinit var binding: FragmentSubscriberListBinding
+    private val viewModel: SubscriberListViewModel by viewModels {
+        object : ViewModelProvider.Factory {
+            override fun <T : ViewModel> create(modelClass: Class<T>): T {
+                val subscriberDao = AppDatabase.getInstance(requireContext()).subscriberDAO
+                val repository: SubscriberRepository = DatabaseDataSource(subscriberDao)
+                return SubscriberListViewModel(repository) as T
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentSubscriberListBinding.bind(view)
 
-        val subscriberListAdapter = SubscriberListAdapter(
-            listOf(
-                SubscriberEntity(1, "Alvaro", "alvaro@alvaro.com"),
-                SubscriberEntity(2, "Diogo", "diogo@diogo.com")
-            )
-        )
-        with(binding.recyclerSubscribers) {
-            setHasFixedSize(true)
-            adapter = subscriberListAdapter
+        observeViewModelEvents()
+
+    }
+
+    private fun observeViewModelEvents() {
+        viewModel.allSubscribersEvent.observe(viewLifecycleOwner){allSubscibers->
+            val subscriberListAdapter = SubscriberListAdapter(allSubscibers)
+
+            with(binding.recyclerSubscribers){
+                setHasFixedSize(true)
+                adapter = subscriberListAdapter
+            }
+
         }
 
     }
--- app/src/main/java/com/prado/crudroom/ui/subscriberlist/SubscriberListViewModel.kt
+++ app/src/main/java/com/prado/crudroom/ui/subscriberlist/SubscriberListViewModel.kt
@@ -1,7 +1,10 @@
 package com.prado.crudroom.ui.subscriberlist
 
 import androidx.lifecycle.ViewModel
+import com.prado.crudroom.repository.SubscriberRepository
 
-class SubscriberListViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SubscriberListViewModel(
+    private val repository: SubscriberRepository
+) : ViewModel() {
+    val allSubscribersEvent = repository.getAllSubscribers()
 }
